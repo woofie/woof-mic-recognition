6db46cf0e1de6c7f41c497e034acfb652093f791
Thomas Wang <dev@paced.me>
2017-08-06 09:02:07 +1000

    POST request to capture server A-OK

--- Assets/KKSpeechRecognizer/Example/RecordingCanvas.cs
+++ Assets/KKSpeechRecognizer/Example/RecordingCanvas.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using KKSpeech;
 using System.Collections.Generic;
 using System.Text;
+using System;
 
 public class RecordingCanvas : MonoBehaviour {
 
@@ -43,7 +44,13 @@ public class RecordingCanvas : MonoBehaviour {
 		} else {
 			WWW req = this.POSTRequest ("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
 										"This is just a test debug string, hopefully I can get some sentiment analysis going.");
+
 			while (!req.isDone) {}
+
+			req = this.POSTRequestGeneral ("https://woof-api.herokuapp.com/api/conversation", "This is just a test debug string, hopefully I can get some sentiment analysis going.", req.text);
+
+			while (!req.isDone) {}
+
 			resultText.text = "RESP: " +  req.text;
 
 			startRecordingButton.enabled = false;
@@ -89,14 +96,21 @@ public class RecordingCanvas : MonoBehaviour {
 
 		/* And also add to the body. */
 		if (command != "") {
-			resultText.text = "COMMAND RECOGNISED: " + command + ", YOU SAID: " + resultText.text;
+			//resultText.text = "COMMAND RECOGNISED: " + command + ", YOU SAID: " + resultText.text;
 		} else {
-			resultText.text = "NO COMMAND, YOU SAID: " + resultText.text;
+			//resultText.text = "NO COMMAND, YOU SAID: " + resultText.text;
 		}
+			
+		while (!req.isDone) {}
+
+		// resultText.text += req.text;
+
+		/* Send the transcript and sentiment to the server, two requests. */
+		req = this.POSTRequestGeneral ("https://woof-api.herokuapp.com/api/conversation", resultText.text, req.text);
 
 		while (!req.isDone) {}
 
-		resultText.text += req.text;
+		resultText.text = req.text;
 	}
 
 	public WWW POSTRequest(string url, string text) {
@@ -113,6 +127,28 @@ public class RecordingCanvas : MonoBehaviour {
 		headers["Content-Type"] = "application/json";
 
 		string str = "{'documents': [{'language': 'en', 'id':'enlpHack5', 'text': '" + text.Replace("'", @"\'") + "'}]}";
+		Debug.Log (str);
+
+		www = new WWW(url, Encoding.ASCII.GetBytes(str), headers);
+		StartCoroutine(WaitForRequest(www));
+
+		return www;
+	}
+
+	public WWW POSTRequestGeneral(string url, string text, string text_large) {
+		WWW www;
+
+		WWWForm form = new WWWForm();
+		Dictionary<string,string> headers = form.headers;
+
+		form.AddField("transcript", text);
+
+		headers["Authorization"] = "Basic d29vZnVzZXI6VU5JSEFDSzIwMTc=";
+		headers["Content-Type"] = "application/json";
+
+		string str = "{'transcript': '" + text.Replace("'", @"\'") + "', 'data': " + text_large.Replace("'", @"\'") + "}";
+		str = str.Replace("'","\"");
+
 		Debug.Log (str);
 
 		www = new WWW(url, Encoding.ASCII.GetBytes(str), headers);
